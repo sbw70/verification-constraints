66050ee35c49e8f89f95c6b323ab597b52445e03
Seth Wells <holiwood4420@gmail.com>
2026-09-23 21:50:17 -0400

    Add files via upload

---
+++ labs/nuvl-edge/esp-local/ESP_LOCAL_007/firmware/main/local_wifi_config.example.h
@@ -0,0 +1,4 @@
+#pragma once
+// Copy to local_wifi_config.h and set your own Wi-Fi password.
+#define ESP_LOCAL_006_WIFI_SSID     "Xer0trust 2.4"
+#define ESP_LOCAL_006_WIFI_PASSWORD "REDACTED_SET_YOUR_OWN"
